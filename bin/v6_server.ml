576afdfe003c489da2624e0758bcaaf247260ff8
John Else <john.else@gmail.com>
2018-04-09 00:10:34 +0100

    Add additional features
    Signed-off-by: John Else <john.else@gmail.com>

--- bin/v6_server.ml
+++ bin/v6_server.ml
@@ -2,12 +2,61 @@ type context = unit
 
 let editions = ["xcp-ng", ("", "", 0)] (* TODO: real editions *)
 
+type additional_feature =
+  | VSwitch
+  | Lab
+  | Stage
+  | StorageLink
+  | StorageLinkSiteRecovery
+  | WebSelfService
+  | WebSelfServiceManager
+  | HotfixApply
+  | ExportResourceData
+  | ReadCaching
+  | CIFS
+  | HealthCheck
+  | XCM
+  | VMMemoryIntrospection
+  | BatchHotfixApply
+  | ManagementOnVLAN
+  | WSProxy
+
+type orientation = Positive | Negative
+
+let keys_of_additional_features =
+  [
+    VSwitch, (Negative, "restrict_vswitch_controller");
+    Lab, (Negative, "restrict_lab");
+    Stage, (Negative, "restrict_stage");
+    StorageLink, (Negative, "restrict_storagelink");
+    StorageLinkSiteRecovery, (Negative, "restrict_storagelink_site_recovery");
+    WebSelfService, (Negative, "restrict_web_selfservice");
+    WebSelfServiceManager, (Negative, "restrict_web_selfservice_manager");
+    HotfixApply, (Negative, "restrict_hotfix_apply");
+    ExportResourceData, (Negative, "restrict_export_resource_data");
+    ReadCaching, (Negative, "restrict_read_caching");
+    CIFS, (Negative, "restrict_cifs");
+    HealthCheck, (Negative, "restrict_health_check");
+    XCM, (Negative, "restrict_xcm");
+    VMMemoryIntrospection, (Negative, "restrict_vm_memory_introspection");
+    BatchHotfixApply, (Negative, "restrict_batch_hotfix_apply");
+    ManagementOnVLAN, (Negative, "restrict_management_on_vlan");
+    WSProxy, (Negative, "restrict_ws_proxy");
+  ]
+
+let additional_params =
+  (* Turn on all additional features. *)
+  List.map
+    (fun (_, (mode, feature_name)) ->
+      (feature_name, string_of_bool (mode = Positive)))
+    keys_of_additional_features
+
 let apply_edition _ dbg edition params =
   let open V6_interface in
   {
     edition = "xcp-ng";
     xapi_params  = Features.(to_assoc_list all_features);
-    additional_params = [];
+    additional_params = additional_params;
     experimental_features = [];
   }
 
